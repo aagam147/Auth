2fdc9fa3ff397f01740c2113d0c902e2acccff4f
Krimal Patel <krimal.p@sgligis.com>
2021-12-21 15:03:46 +0530

    first commit

---
+++ CookieAuthenticationInAsp.NetCore-master/CookieDemo/Startup.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace CookieDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(opy =>
+            {
+                opy.ExpireTimeSpan = System.TimeSpan.FromSeconds(10);
+            }).AddGoogle(options =>
+            {
+                options.ClientId = "732213431988-tmgqpfic91q0oqelcd9h04c2o3io3fkp.apps.googleusercontent.com";
+                options.ClientSecret = "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";
+                options.CallbackPath = "/Home/Index";
+                options.ReturnUrlParameter = "https://localhost:44386/";
+                options.RemoteAuthenticationTimeout = System.TimeSpan.FromSeconds(100);
+                options.SaveTokens = true;
+                options.TokenEndpoint = "https://oauth2.googleapis.com/token";
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //for SSL
+            //services.Configure<MvcOptions>(options =>
+            //{
+            //    options.Filters.Add(new RequireHttpsAttribute());
+            //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
+        }
+    }
+}
